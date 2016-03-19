e0379c9b6ceed6aee547f6e477802ba8d79c3c4a
jkurra <jarmo.kurra@gmail.com>
2016-03-19 15:26:01 +0200

    Add proper widget closing and error messages.
    Refactored model functions to report null pointers and added proper
    way to close views in model_close_view function. Also added function
    descriptions to model.h

--- model.c
+++ model.c
@@ -2,56 +2,71 @@
 
 void model_init_view( view_model *model )
 {
-	switch( model->type ) {
-		case MENU:{
-			g_print("MODEL [INIT] : menu\n");
-			view_menu_init( model );
-			break; }
-		case GAME:{
-			g_print("MODEL [INIT] : game\n");
-			view_game_init( model );
-			break;}
-		case PREF:{
-			g_print("MODEL [INIT] : pref\n");
-			view_pref_init( model );
-			break;}
-		default:
-			break;
-	}
+	if(model) {
+		switch( model->type ) {
+			case MENU:{
+				printf("MODEL [INIT] : menu\n");
+				view_menu_init( model );
+				break; }
+			case GAME:{
+				printf("MODEL [INIT] : game\n");
+				view_game_init( model );
+				break;}
+			case PREF:{
+				printf("MODEL [INIT] : pref\n");
+				view_pref_init( model );
+				break;}
+			default:
+				break;
+		}
+	} else { printf("MODEL [INIT] : ERROR! Received null pointer to model\n"); }
 }
 
 void model_draw_view( view_model *model )
 {
-	switch( model->type ) {
-		case MENU:
-			gtk_widget_queue_draw( model->menu->main_frame );
-			break;
-		case GAME:
-			gtk_widget_queue_draw( model->game->main_frame );
-			break;
-		case PREF:
-			gtk_widget_queue_draw( model->pref->main_frame );
-			break;
-		default:
-			break;
-	}
+	if(model) {
+		switch( model->type ) {
+			case MENU:
+				gtk_widget_queue_draw( model->menu->main_frame );
+				break;
+			case GAME:
+				gtk_widget_queue_draw( model->game->main_frame );
+				break;
+			case PREF:
+				gtk_widget_queue_draw( model->pref->main_frame );
+				break;
+			default:
+				break;
+		}
+	} else { printf("MODEL [DRAW] : ERROR! Received null pointer to model\n"); }
 }
 
 void model_close_view( view_model *model )
 {
-	switch( model->type ) {
-		case MENU:
-			gtk_widget_destroy( GTK_WIDGET(model->menu->main_frame) );
-			break;
-		case GAME:
-			gtk_widget_destroy( GTK_WIDGET(model->game->main_frame) );
-			break;
-		case PREF:
-			gtk_widget_destroy( GTK_WIDGET(model->pref->main_frame) );
-			break;
-		default:
-			break;
-	}
+	if(model) {
+		switch( model->type ) {
+			case MENU:
+				printf("MODEL [CLOSE] : menu\n");
+				g_object_ref_sink(G_OBJECT(model->menu->main_frame));
+				gtk_widget_destroy(GTK_WIDGET(model->menu->main_frame));
+				g_object_unref(G_OBJECT(model->menu->main_frame ));
+				break;
+			case GAME:
+				printf("MODEL [CLOSE] : game\n");
+				g_object_ref_sink(G_OBJECT(model->game->main_frame));
+				gtk_widget_destroy( GTK_WIDGET(model->game->main_frame));
+				g_object_unref(G_OBJECT(model->game->main_frame));
+				break;
+			case PREF:
+				printf("MODEL [CLOSE] : pref\n");
+				g_object_ref_sink(G_OBJECT(model->pref->main_frame));
+				gtk_widget_destroy( GTK_WIDGET(model->pref->main_frame) );
+				g_object_unref(G_OBJECT(model->pref->main_frame));
+				break;
+			default:
+				break;
+		}
+	} else { printf("MODEL [CLOSE] : ERROR! Received null pointer to model\n"); }
 }
 
 void model_update( view_model *model, int type )
@@ -60,8 +75,9 @@ void model_update( view_model *model, int type )
 		case MENU:
 			break;
 		case GAME: {
+
 			char *json = jsm_read(model->pref_path);
-			printf("JSM [PARSE] : %s : ", json);
+			//printf("JSM [PARSE] : %s : ", json);
 			if(json) {
 				char *tick_time, *x_size, *y_size, *bgCol, *frCol,*infi, *visi; /* free these */
 				tick_time = jsm_json_val( json, "TICK_TIME", 3 );
@@ -137,12 +153,18 @@ void model_rwrite( view_model *model, int type)
 			sprintf(t_time, "%d", model->game->tick_t);
 			sprintf(vis, "%d", model->game->visible);
 
+			gchar *bgrn = gdk_rgba_to_string(&model->game->bgrn_col);
+			gchar *cell = gdk_rgba_to_string(&model->game->cell_col);
+
 			json = jsm_json_add(json, "X_SIZE", &x_size);
 			json = jsm_json_add(json, "Y_SIZE", &y_size);
 			json = jsm_json_add(json, "TICK_TIME", &t_time);
 			json = jsm_json_add(json, "gridVis", &vis);
-			json = jsm_json_add(json, "bgColor", gdk_rgba_to_string(&model->game->bgrn_col));
-			json = jsm_json_add(json, "frColor", gdk_rgba_to_string(&model->game->cell_col));
+			json = jsm_json_add(json, "bgColor", bgrn);
+			json = jsm_json_add(json, "frColor", cell);
+
+			free(bgrn);
+			free(cell);
 
 			int s = jsm_write(json, model->pref_path);
 			if(s != JSM_OK) {
--- model.h
+++ model.h
@@ -3,23 +3,49 @@
 
 #include "view.h"
 #include "jsmn/jsm.h"
-#include <stdlib.h>
+
+/* Types of views that may be used */
 #define MENU  0
 #define GAME  1
 #define PREF  2
 
 /** @brief Initialize current model.
  *
+ * Initializes view using view_model, which contains type value. Type value is
+ * used to determine which model is to be initialized. Therefore it must be set
+ * before calling this function, otherwise behaviour may be unexpected. It is
+ * also good to notice that this function doesn't free views that may be
+ * initialized, so that must be taken care of before calling this function to
+ * avoid double assingments of widgets. This function is meant to be used when
+ * it is necessary to initialize view using view_model. This way all necessary
+ * data is present when values are assigned.
+ *
+ * @param model Contains data needed to initialize view.
  */
 void model_init_view( view_model *model );
 
 /** @brief Draw current model.
  *
+ * Adds selected view to gtk-widget draw queue. Gives quick way of updating the
+ * view without having to select which view needs to be drawn. As long as
+ * view_model contains correct view and type, it can automatically select right
+ * update function.
+ *
+ * @param model Contains view to be updated.
  */
 void model_draw_view( view_model *model );
 
 /** @brief Close current model.
  *
+ * unrefs and calls destroy on currently active views main widget. Notice that
+ * type must point to correct widget type or it may try to release incorrect
+ * view. Make sure to call this before changing widget type and calling init_view.
+ * Purpose of this function is to provide quick way of freeing view. However since
+ * view_model contains multiple sub-views it is up to user to make sure that
+ * correct view is selected. In future more checks may be assigned to avoid
+ * mistakes.
+ *
+ * @param model Contains view to be closed.
  */
 void model_close_view( view_model *model );
 
